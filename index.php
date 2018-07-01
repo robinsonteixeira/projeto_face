dae2f0816c2f6ae40ec31f09c2c45f72e875bf36
Robinson Teixeira <robinson_teixeira1@hotmail.com>
2018-07-01 02:16:06 -0300

    Primeira atualização

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Replicando o Face</title>
+	<script type="text/javascript"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<meta charset="utf-8">
+</head>
+<body>
+	<header>
+		<div class="menu_topo">
+			<div class="menu_le">
+				
+					<div class="icon_face">
+						<a href=""><img src="imagens/icon_logoface.png"></a>
+					</div>
+					<div class="pesquisa">
+						<input type="text" name="pesquisa" placeholder="Pesquisar"/>
+					</div>
+					<div class="lupa">
+						<a href=""><img src="imagens/lupa.png"/></a>
+					</div>
+					<div class="rosto">
+						<a href=""><img src="imagens/rosto.png"/></a>
+					</div>
+					<div class="rosto_nome">
+						<a href=""><strong>Robinson</strong></a>
+					</div>
+					<div class="barra">
+
+					</div>
+			</div>
+			<div class="menu_ld">
+				<div class="icon_mudarconta">
+					<a href=""><img src="imagens/icon_mudarconta.png"/></a>
+				</div>
+				<div class="barra0">
+
+				</div>
+				<div class="pagina_inicial">
+					<a href=""><strong>Página inicial</strong></a>
+				</div>
+				<div class="barra1">
+
+				</div>
+				<div class="icon_solicitacaoamizade">
+					<a href=""><img src="imagens/icon_solicitaçaoamizade.png"/></a>
+				</div>
+				<div class="icon_messenger">
+					<a href=""><img src="imagens/icon_messenger.png"/></a>
+				</div>
+				<div class="icon_notificacao">
+					<a href=""><img src="imagens/icon_notificaçao.png"/></a>
+				</div>
+				<div class="icon_ajudarapida">
+					<a href=""><img src="imagens/icon_ajudarapida.png"/></a>
+				</div>
+				<div class="icon_config">
+					<a href=""><img src="imagens/icon_config.png"/></a>
+				</div>
+			</div>		
+		</div>
+	</header>
+	<article>
+		<div class="container">
+			<div class="container_le">
+
+				<div class="container_le_01">
+
+				</div>
+				<div class="container_le_02">
+					<div class="container_le_02a">
+						<div class="container_le_02a1">
+						</div>
+						<div class="container_le_02a2">
+						</div>
+						<div class="container_le_02a3">
+						</div>
+					</div>
+					<div class="container_le_02b">
+						<div class="container_le_02b1">
+							<div class="container_le_02b1a">	
+								<a href=""><img src="imagens/G1.png"/></a>
+							</div>
+							<div class="container_le_02b1b">
+								<span><strong>G1 - O portal de notícias da Globo</strong></span>
+							</div>
+							<div class="container_le_02b1c">
+								<div class="container_le_02b1c1">
+									<span>Missa aos domingos.</span>
+								</div>
+							</div>	
+						</div>
+						<div class="container_le_02b2">
+							<a href=""><img src="imagens/foto_noticia.png" width="497px" height="260px"></a>
+						</div>
+						<div class="container_le_02b3">
+						</div>
+						<div class="container_le_02b4">
+						</div>
+						<div class="container_le_02b5">
+						</div>
+					</div>
+				</div>
+				<div class="container_le_03">
+
+				</div>
+			</div>
+			<div class="container_ld">
+				<div class="container_ld_01">
+
+				</div>
+			</div>
+		</div>
+	</article>	
+</body>
+</html>
